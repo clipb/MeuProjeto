b774e4cefdec823f41c1ff842a4bb6be1a65abe5
Leandro <leandroplus70@yahoo.com.br>
2025-08-18 09:54:44 -0300

    Trabalho da aula passada...

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+# calcular as prestações do produto...
+valor = int(input("digite o valor do produto:"))
+quantiddade = int(input("Digite a quantidade de parcelas: "))
+parcelas = valor / quantiddade
+print("o valor da parcela será R$", parcelas)
+
+
+
---
+++ experimental.py
@@ -0,0 +1,1 @@
+#Adicionei# o arquivo Experimental
