1683225e1286af90ee4d4f204d1f2c75c6b155e4
Leandro <leandroplus70@yahoo.com.br>
2025-08-18 10:02:44 -0300

    atividade da aula

---
+++ expressao_matematica.py
@@ -0,0 +1,11 @@
+# Demontração de expreões complexas...
+x = 10; y = 20; z = 30
+
+VALOR = x * (y / 10 + z / 10) - 25
+print("o resultado da expressão #1 é:", VALOR)
+
+VALOR((y - x)+(z - x)) / 6
+print("o resultado da expressão #2 é:", VALOR)
+
+VALOR(x + y(y * 2 + (z / 7))) - 18
+print("o reusultado da expressão #3 é:", VALOR)
